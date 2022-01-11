8d8ba8d6f0d869d2b4bfca29c506eb7825c7c962
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2022-01-11 07:27:00 -0300

    as

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, Djaeeeeeingo!")
+    return HttpResponse("Hello, Django!")
 
 def hello_there(request, name):
     now = datetime.now()
